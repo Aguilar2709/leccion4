b985a364217b819a1bbf2fa3ebe0ce6b47d7168f
Aguilar2709 <124844498+Aguilar2709@users.noreply.github.com>
2023-02-16 02:46:51 +0000

    leccion4

---
+++ isograma.py
@@ -0,0 +1,11 @@
+palabra="centrifugado"
+acumulador = 0
+for indice,palabra in enumerate(palabra):
+    print(indice,palabra)
+    acumulador = acumulador + 1
+
+print(acumulador)
+
+
+
+
--- test_main.py
+++ test_main.py
@@ -1,6 +1,19 @@
-def func(x):
-    return x + 1
+   
+
+palabra="leonardo"
+#print(palabra[0])
+#print(palabra[1])
+#print(palabra[2])
+#print(palabra[3])
+#print(palabra[4])
+#print(palabra[5])
+#print(palabra[6])
+indice=0  
+for letra in palabra:
+    print(indice,letra)
+    indice=indice+1
+for indice, letra in enumerate(palabra):
+    print(indice,letra)
 
 
-def test_answer():
-    assert func(3) == 5
+
